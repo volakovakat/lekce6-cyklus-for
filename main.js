d4e308a769aa81455ddbf33cd1a36b6ae4d88b00
kaczenka <75174671+kaczenka@users.noreply.github.com>
2021-04-17 10:48:29 +0200

    přepis na while

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
-// Použij výpis do konzole prohlížeče pomocí console.log('text')
-// Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
-
-
-// První příklad - vypiš vzestupně čísla od 0 do 10
+//1. příklad
 console.log('První příklad - vzestupná řada 0 - 10');
-
-//TADY NAPIS CYKLUS
-
+//WHILE
+let i = 0;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
 console.log('-------------------');
 
-
-
-// Druhý příklad - zkopíruj předchozí příklad
+// 2. příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
+//WHILE
+i = -1;
+while (i < 10) {
+    i++;
+    console.log(i);
+}
 
 console.log('-------------------');
 
-
-
-// Třetí příklad - vypiš sestupně čísla od 10 do 0
+//3. příklad
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
+//WHILE
+i = 10;
+while (i >= 0) {
+    console.log(i);
+    i--;
+}
 
 console.log('-------------------');
